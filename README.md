efb117b0a2af8217f32fef641254f457a13bc0da
zxc <zxc90919zxc@gmail.com>
2021-06-25 11:37:24 +0800

    上傳

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 123ts
+https://karta1252839.github.io/123ts/index.html
+派大金2.0
---
+++ js/history.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    let title = $('title').text();
+    $('#copyTitle').html(title);
+});
